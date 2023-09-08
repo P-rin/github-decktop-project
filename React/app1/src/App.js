aa3102c465418868350c7e997501431fc1778e4a
P-rin <actprin@gmail.com>
2023-09-08 23:34:58 +0700

    แก้งาน

--- React/app1/src/App.js
+++ React/app1/src/App.js
@@ -1,15 +1,13 @@
-import React from "react";
-import {userContext} from './context'
-import Header from './context-header'
-import Content from './context-content'
+import React from 'react'
+import { userContext } from './context'
+import  Header2  from './context-header2'
+import  Content2  from './context-content2'
 
-
-export  default function App(){
+export default function App(){
+  let [user,setUser] = React.useState()
   return (
-    <userContext.Provider value={'Tom Jerry'}>
-      <Header/>
-      <Content/>
-    </userContext.Provider>
+  <userContext.Provider value={[user,setUser]}>
+    <Header2/><Content2/>
+  </userContext.Provider>
   )
 }
-
--- React/app1/src/context-header.js
+++ React/app1/src/context-header.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import {userContext} from './context'
+import { userContext } from './context'
 
-export default class Header extends React.Component{
+export default class Header extends React.Component {
     static contextType = userContext
+
     render(){
         const user = this.context
+
         const headerStyle = {
             backgroundColor:'#cee',
             textAlign:'center',
-            padding:5
+            padding: 5 
         }
         return(
-            <div style={headerStyle}>
+            <div style = {headerStyle}>
                 <a href=" ">Home</a>&nbsp;-&nbsp;
                 <a href=" ">Product</a>&nbsp;-&nbsp;
-                <a href=" ">Contact</a>&nbsp;-&nbsp;
-                [{user}&nbsp;&nbsp;<a href=" ">Signout</a>]
+                <a href=" ">Contact Us</a>&nbsp;-&nbsp;&nbsp;
+                [{user}&nbsp;:&nbsp;<a href=" ">Signout</a>]
             </div>
         )
     }
